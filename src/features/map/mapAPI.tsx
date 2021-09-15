31260bb68d5b2353dc9329c479fb328ba63aff47
zeritte <zeritte@gmail.com>
2021-09-15 02:41:09 +0100

    add in counter

--- src/features/map/mapAPI.tsx
+++ src/features/map/mapAPI.tsx
@@ -1,5 +1,36 @@
 import data from '../../data.json';
+import { Size } from './mapSlice';
 
 export function fetchMapData() {
   return new Promise<{ data: any }>((resolve) => setTimeout(() => resolve({ data }), 500));
+}
+
+export function counter(data) {
+  const rampsPerMaterialCount = [];
+  const rampsPerSize = [
+    { size: Size.zero_fifty, count: 0 },
+    { size: Size.fifty_twohundred, count: 0 },
+    { size: Size.twohundred_infinity, count: 0 }
+  ];
+  for (let i = 0; i < data?.features?.length; i++) {
+    const material = data.features[i].properties.material;
+    const area = data.features[i].properties.area_;
+    const materialIndex = rampsPerMaterialCount.findIndex((r) => r.name === material);
+
+    if (materialIndex > -1) {
+      rampsPerMaterialCount[materialIndex].count += 1;
+    } else {
+      rampsPerMaterialCount.push({ name: material, count: 1 });
+    }
+
+    if (area < 50) {
+      rampsPerSize[0].count += 1;
+    } else if (area < 200) {
+      rampsPerSize[1].count += 1;
+    } else {
+      rampsPerSize[2].count += 1;
+    }
+  }
+
+  return [rampsPerMaterialCount, rampsPerSize];
 }
