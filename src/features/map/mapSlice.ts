4612ffe8069b045ec131f231a10a6e846f7d8cba
zeritte <zeritte@gmail.com>
2021-09-15 02:40:57 +0100

    do lots of filtering and reducers

--- src/features/map/mapSlice.ts
+++ src/features/map/mapSlice.ts
@@ -1,15 +1,16 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction, current } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
-import { fetchMapData } from './mapAPI';
+import { counter, fetchMapData } from './mapAPI';
 
 type ValueOf<T> = T[keyof T];
-const Size = {
+export const Size = {
   zero_fifty: '0-50',
   fifty_twohundred: '50-200',
   twohundred_infinity: '200-more'
 };
 
 export interface CounterState {
+  rawData: any;
   data: any;
   status: 'idle' | 'loading' | 'failed';
   rampsPerMaterial: { name: string; count: number }[];
@@ -17,6 +18,7 @@ export interface CounterState {
 }
 
 const initialState: CounterState = {
+  rawData: null,
   data: null,
   status: 'idle',
   rampsPerMaterial: [],
@@ -27,11 +29,6 @@ const initialState: CounterState = {
   ]
 };
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 export const fetchMapDataAsync = createAsyncThunk('map/fetchMapData', async () => {
   const response = await fetchMapData();
   // The value we return becomes the `fulfilled` action payload
@@ -41,7 +38,20 @@ export const fetchMapDataAsync = createAsyncThunk('map/fetchMapData', async () =
 export const mapSlice = createSlice({
   name: 'map',
   initialState,
-  reducers: {},
+  reducers: {
+    filterOnZoom: (state, action: PayloadAction<[number, number, number, number]>) => {
+      const rawData = current(state.rawData);
+      const filteredData = rawData.features?.filter((t) => {
+        const coordinates = t.geometry.coordinates[0][0][0];
+        // only does east and west for now
+        return coordinates[0] > action.payload[0] && coordinates[0] > action.payload[3];
+      });
+      state.data.features = filteredData;
+      const [rampsPerMaterialCount, rampsPerSize] = counter(state.data);
+      state.rampsPerMaterial = rampsPerMaterialCount;
+      state.rampsPerSize = rampsPerSize;
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchMapDataAsync.pending, (state) => {
@@ -49,32 +59,10 @@ export const mapSlice = createSlice({
       })
       .addCase(fetchMapDataAsync.fulfilled, (state, action) => {
         state.status = 'idle';
+        state.rawData = action.payload;
         state.data = action.payload;
-        const rampsPerMaterialCount = [];
-        const rampsPerSize = [
-          { size: Size.zero_fifty, count: 0 },
-          { size: Size.fifty_twohundred, count: 0 },
-          { size: Size.twohundred_infinity, count: 0 }
-        ];
-        for (let i = 0; i < action.payload?.features?.length; i++) {
-          const material = action.payload.features[i].properties.material;
-          const area = action.payload.features[i].properties.area_;
-          const materialIndex = rampsPerMaterialCount.findIndex((r) => r.name === material);
 
-          if (materialIndex > -1) {
-            rampsPerMaterialCount[materialIndex].count += 1;
-          } else {
-            rampsPerMaterialCount.push({ name: material, count: 1 });
-          }
-
-          if (area < 50) {
-            rampsPerSize[0].count += 1;
-          } else if (area < 200) {
-            rampsPerSize[1].count += 1;
-          } else {
-            rampsPerSize[2].count += 1;
-          }
-        }
+        const [rampsPerMaterialCount, rampsPerSize] = counter(action.payload);
 
         state.rampsPerMaterial = rampsPerMaterialCount;
         state.rampsPerSize = rampsPerSize;
@@ -82,7 +70,7 @@ export const mapSlice = createSlice({
   }
 });
 
-export const {} = mapSlice.actions;
+export const { filterOnZoom } = mapSlice.actions;
 
 export const selectMapData = (state: RootState) => state.map.data;
 export const selectRampsPerMaterial = (state: RootState) => state.map.rampsPerMaterial;
